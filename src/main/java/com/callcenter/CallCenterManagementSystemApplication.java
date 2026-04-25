cfaa468958eecced81c2c74ac9a116e8f396bcec
Sree Harsha <sriharshakakani@gmail.com>
2026-04-24 20:03:50 -0500

    Initial spring boot setup for call center management system.

---
+++ src/main/java/com/callcenter/CallCenterManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallCenterManagementSystemApplication.class, args);
+    }
+
+}
